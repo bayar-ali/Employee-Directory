1bca70b3a61fbd3cb9aaa05c1bb53e537b7fc73e
unknown <rashadbayar@gmail.com>
2020-07-21 21:27:55 -0700

    Updates

---
+++ precache-manifest.1b59a58bb4852ffd057490b44891f9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f84930d876f58bd1c6a78e98c600031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2946682e20c03a2d6a81",
+    "url": "/static/css/main.e48e1a4d.chunk.css"
+  },
+  {
+    "revision": "abe11e4eb9023bd6a56d",
+    "url": "/static/js/2.d9395e57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9395e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2946682e20c03a2d6a81",
+    "url": "/static/js/main.98990650.chunk.js"
+  },
+  {
+    "revision": "bdf2842d60398c1f91d7",
+    "url": "/static/js/runtime-main.6e1dd78f.js"
+  }
+]);
